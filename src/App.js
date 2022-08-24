21c958cd5b379af88f1414048e340857edb48ab4
Milind <milind@samco.in>
2022-08-24 10:41:28 +0530

    added header and footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Alert from "@mui/material/Alert";
 import Button from "@mui/material/Button";
+import DrawerAppBar from "./components/NavBar";
+import Footer from "./components/Footer";
 const url = "https://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
@@ -38,40 +40,58 @@ function App() {
 
   if (loading) {
     return (
-      <Container maxWidth="sm" sx={{ mt: 4 }}>
-        <Typography variant="h3" gutterBottom>
-          Loading...
-        </Typography>
-      </Container>
+      <>
+        <DrawerAppBar />
+        <Container maxWidth="sm" sx={{ mt: 14 }}>
+          <Typography variant="h3" gutterBottom>
+            Loading...
+          </Typography>
+        </Container>
+        <Footer />
+      </>
     );
   }
 
   if (errorFlag) {
     return (
-      <Container maxWidth="sm" sx={{ mt: 4 }}>
-        <Alert severity="error">Data not available.</Alert>
-      </Container>
+      <>
+        <DrawerAppBar />
+        <Container maxWidth="sm" sx={{ mt: 14 }}>
+          <Alert severity="error">Data not available.</Alert>
+        </Container>
+        <Footer />
+      </>
     );
   }
 
   if (tours.length === 0) {
     return (
-      <Container maxWidth="sm" align="center" sx={{ mt: 4 }}>
-        <Typography variant="h5" gutterBottom>
-          No tours found
-        </Typography>
-        <Button variant="outlined" onClick={() => fetchTours()} sx={{ mt: 4 }}>
-          Reset
-        </Button>
-      </Container>
+      <>
+        <DrawerAppBar />
+        <Container maxWidth="sm" align="center" sx={{ mt: 14 }}>
+          <Typography variant="h3" gutterBottom>
+            No tours found
+          </Typography>
+          <Button
+            variant="outlined"
+            onClick={() => fetchTours()}
+            sx={{ mt: 4, mb: 8 }}
+          >
+            Reset
+          </Button>
+        </Container>
+        <Footer />
+      </>
     );
   }
 
   return (
     <>
-      <Container maxWidth="sm" sx={{ mt: 4 }}>
+      <DrawerAppBar />
+      <Container maxWidth="sm" sx={{ mt: 14 }}>
         <Tours tours={tours} removeTour={removeTour} />
       </Container>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#1976d2",
+        color: "white",
+        padding: "16px",
+        textAlign: "center",
+      }}
+    >
+      by milind.k
+    </footer>
+  );
+};
+
+export default Footer;
